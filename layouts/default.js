f9307bb49f3f0dbc0d94b80d55ed10543123afb4
unknown <olawanletemitayo@gmail.com>
2021-11-24 05:04:27 +0100

    made some changes

--- layouts/default.js
+++ layouts/default.js
@@ -1,12 +1,14 @@
-import Head from 'next/head';
-import styles from '../styles/default.module.css';
+/* eslint-disable quotes */
+import Head from "next/head";
+import styles from "../styles/default.module.css";
 
 const Default = ({ children }) => (
   <div className={styles.container}>
     <Head>
       <title>CrowFunding</title>
+      <link rel="icon" href="/images/favicon-32x32.png" />
     </Head>
-    { children }
+    {children}
   </div>
 );
 
